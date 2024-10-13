476606efcbffca84325d0dc2e54117fae460ea37
Vanhoras <phielsche@t-online.de>
2024-10-13 18:26:48 +0200

    Added sprites and player prefab

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private PlayerInputActions inputActions;
-    private NavMeshAgent agent;
-
-    private Vector2 followSpot;
-    private bool moving;
-    private bool doNotMove;
-
-    private Vector3 lastPosition;
-    private float checkLastPositionEvery = 0.1f;
-    private float timeTillCheckPosition;
 
     private PlayerParent playerParent;
     private GameObject dummyTopScale;
@@ -25,85 +15,25 @@ public class Player : MonoBehaviour
         dummyTopScale = playerParent.dummyTopScale;
         dummyBottomScale = playerParent.dummyBottomScale;
 
-        if (playerParent.reflection)
-        {
-            gameObject.transform.Find("shadow").gameObject.SetActive(false);
-            gameObject.transform.Find("reflection").gameObject.SetActive(true);
-        }
-
         inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
         inputActions.Player.Click.performed += OnMouseClick;
 
-        agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-
-        agent.speed *= playerParent.speedModifier;
-
-        lastPosition = transform.position;
-
         AdjustPerspective();
-
-        InventoryManager.instance.LanternActivated += ActivateLantern;
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (!moving) return;
-
-        AdjustPerspective();
-        ChangeDirection();
-
-        if (AgentHasStopped())
-        {
-            moving = false;
-        }
+        
     }
 
     private void OnMouseClick(InputAction.CallbackContext input)
     {
         Vector2 inputVector = inputActions.Player.Position.ReadValue<Vector2>();
-        FindInteractible(inputVector);
-
-        if (!doNotMove && !StopManager.instance.stopped)
-        {
-            MoveStart(inputVector);
-        }
-        doNotMove = false;
     }
-
-    private void MoveStart(Vector2 inputVector)
-    {
-        moving = true;
-
-        followSpot = Camera.main.ScreenToWorldPoint(inputVector);
-
-        Vector3 destination = new Vector3(followSpot.x, followSpot.y, transform.position.z);
-        agent.SetDestination(destination);
-    }
-
-    private void FindInteractible(Vector2 inputVector)
-    {
-
-        Ray ray = Camera.main.ScreenPointToRay(inputVector);
-        RaycastHit2D[] hits = Physics2D.RaycastAll(ray.origin, ray.direction);
-
-        foreach (RaycastHit2D hit in hits)
-        {
-            if (hit.collider == null)
-            {
-                continue;
-            }
-            GameObject hitObject = hit.transform.gameObject;
-            if (hitObject == null)
-            {
-                continue;
-            }
-        }
-    }
+    
 
     private void AdjustPerspective()
     {
@@ -117,41 +47,13 @@ public class Player : MonoBehaviour
         transform.localScale = Vector3.Lerp(topScale, bottomScale, 1 - percentY);
     }
 
-    private void ChangeDirection()
+    private void ChangeDirection(bool left)
     {
-
-        Vector3 directionVector = transform.position - lastPosition;
-
-        if (directionVector.x == 0) return;
-
-        bool left = directionVector.x < 0;
-
         transform.rotation = left ? transform.rotation = Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
-    }
-
-    private bool AgentHasStopped()
-    {
-        timeTillCheckPosition += Time.deltaTime;
-
-        if (timeTillCheckPosition < checkLastPositionEvery) return false;
-
-        timeTillCheckPosition = 0;
-
-        bool hasStopped = transform.position == lastPosition;
-
-        lastPosition = transform.position;
-
-        return hasStopped;
-    }
-
-    public void ActivateLantern()
-    {
-        transform.Find("lantern").gameObject.SetActive(true);
     }
 
     private void OnDestroy()
     {
         inputActions.Player.Click.performed -= OnMouseClick;
-        InventoryManager.instance.LanternActivated -= ActivateLantern;
     }
 }
